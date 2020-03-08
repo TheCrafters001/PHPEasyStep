46fa205d57eeb448ce4cade3080ba57e8a42098d
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-03-07 20:19:08 -0500

    Create index.php

---
+++ Image of the day/index.php
@@ -0,0 +1,49 @@
+<?php
+
+$today=date(l);
+
+// Find what today is? using date function
+
+if($today==Monday){
+echo "Today is Monday";
+echo "<BR>";
+echo "<img src='images/image1.gif'>";
+}
+
+elseif($today==Tuesday){
+echo "Today is Tuesday";
+echo "<BR>";
+echo "<img src='images/image2.gif'>";
+}
+
+elseif($today==Wednesday){
+echo "Today is Wednesday";
+echo "<BR>";
+echo "<img src='images/image3.gif'>";
+}
+
+elseif($today==Thursday){
+echo "Today is Thursday";
+echo "<BR>";
+echo "<img src='images/image4.gif'>";
+}
+
+elseif($today==Friday){
+echo "Today is Friday";
+echo "<BR>";
+echo "<img src='images/image5.gif'>";
+}
+
+elseif($today==Saturday){
+echo "Today is Saturday";
+echo "<BR>";
+echo "<img src='images/image6.gif'>";
+}
+
+elseif($today==Sunday){
+echo "Today is Sunday";
+echo "<BR>";
+echo "<img src='images/image7.gif'>";
+}
+
+?>
