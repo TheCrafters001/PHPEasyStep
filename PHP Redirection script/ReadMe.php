0e0e622d913ceb8f5b0b206c6f45b57b14d9384f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-03-07 20:59:01 -0500

    Create ReadMe.php

---
+++ PHP Redirection script/ReadMe.php
@@ -0,0 +1,10 @@
+# PHP Redirection script
+Learn how to redirect in this tutorial, you can use header(); function in php or use meta to redirect to a new page you want or redirect to other website. In this scripts, you can use header(); method or use meta to redirect.
+## Code - PHP Redirection script
+```php
+<?php header("location:http://www.phpeasystep.com"); ?>
+```
+or
+```php
+<?php echo "<meta http-equiv='refresh' content='0;url=http://www.phpeasystep.com'>";
+```
